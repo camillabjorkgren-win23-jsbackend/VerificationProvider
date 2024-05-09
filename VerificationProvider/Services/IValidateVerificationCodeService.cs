8fba59e2689fa01ead92e9e5ef502d29ba7af45f
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-09 16:47:25 +0200

    validateverificationcode completed and tested

---
+++ VerificationProvider/Services/IValidateVerificationCodeService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services;
+public interface IValidateVerificationCodeService
+{
+    Task<ValidateRequest> UnpackValidateRequestAsync(HttpRequest req);
+    Task<bool> ValidateCodeAsync(ValidateRequest validateRequest);
+}
