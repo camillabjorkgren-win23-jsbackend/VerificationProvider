17e8003dd7b66f2582b270952460cf4decd4bd19
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-09 12:58:16 +0200

    verificationcode completed

---
+++ VerificationProvider/Data/Context/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerificationProvider.Data.Entities;
+
+namespace VerificationProvider.Data.Context;
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+
+    public DbSet<VerificationRequestEntity> VerificationRequests { get; set; }
+}
