135cad6823ad95443d698cff71c1cf0715f8162a
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-13 19:36:57 +0200

    test

--- VerificationProvider/Functions/GenereteVerificationCodeHTTP.cs
+++ VerificationProvider/Functions/GenereteVerificationCodeHTTP.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using VerificationProvider.Models;
 using VerificationProvider.Services;
@@ -28,46 +29,72 @@ public class GenereteVerificationCodeHTTP
     [Function("GenereteVerificationCodeHTTP")]
     public async Task<string> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
     {
-        var body = await new StreamReader(req.Body).ReadToEndAsync();
-        if (!string.IsNullOrWhiteSpace(body))
-            try
+        try
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            if (!string.IsNullOrWhiteSpace(body))
             {
-                var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(body.ToString());
-                if (verificationRequest != null && !string.IsNullOrEmpty(verificationRequest.Email))
-                {
-                    var code = _verificationService.GenerateCode();
-                    if (!string.IsNullOrEmpty(code))
-                    {
-                        var result = await _verificationService.SaveVerificationRequest(verificationRequest, code);
 
-                        if (result)
+                try
+                {
+                    var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(body.ToString());
+                    if (verificationRequest != null && !string.IsNullOrEmpty(verificationRequest.Email))
+                    {
+                        try
                         {
-                            var emailRequest = _verificationService.GenerateEmailRequest(verificationRequest, code);
-                            if (emailRequest != null)
+                            var code = _verificationService.GenerateCode();
+                            if (!string.IsNullOrEmpty(code))
                             {
-                                var payload = _verificationService.GenerateServiceBusEmailRequest(emailRequest);
-                                if (!string.IsNullOrEmpty(payload))
+                                var result = await _verificationService.SaveVerificationRequest(verificationRequest, code);
+                                if (result)
                                 {
-                                    var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new
+                                    var emailRequest = _verificationService.GenerateEmailRequest(verificationRequest, code);
+                                    if (emailRequest != null)
                                     {
-                                        Email = verificationRequest.Email,
-                                    })));
-                                    await _queueClient.SendAsync(message);
-                                    _logger.LogInformation("Message sent to Service Bus queue");
-                                    return payload;
+                                        try
+                                        {
+                                            var payload = _verificationService.GenerateServiceBusEmailRequest(emailRequest);
+                                            if (!string.IsNullOrEmpty(payload))
+                                            {
+                                                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new
+                                                {
+                                                    Email = verificationRequest.Email,
+                                                })));
+                                                await _queueClient.SendAsync(message);
+                                                _logger.LogInformation("Message sent to Service Bus queue");
+                                                return payload;
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            _logger.LogError($"ERROR : GenereteVerificationCodeHTTP.Run() :: {ex.Message}");
+                                        }
+
+                                    }
                                 }
                             }
                         }
-
+                        catch (Exception ex)
+                        {
+                            _logger.LogError($"ERROR : GenereteVerificationCodeHTTP.Run() :: {ex.Message}");
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"ERROR : GenereteVerificationCodeHTTP.Run() :: {ex.Message}");
+                }
+
             }
-            catch (Exception ex)
-            {
-                _logger.LogError($"ERROR : GenereteVerificationCodeHTTP.Run() :: {ex.Message}");
-            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenereteVerificationCodeHTTP.Run() :: {ex.Message}");
+        }
+
         return null!;
     }
+
 
 
 
